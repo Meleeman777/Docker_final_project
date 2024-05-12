c1006119501a16604ef4fb431c7ef0c7f6a7f059
Ivan Abramenko <meleemanhs@gmail.com>
2024-05-12 10:57:32 +0200

    3.1

--- up.sh
+++ up.sh
@@ -2,4 +2,3 @@
 
 docker stop $(docker ps -aq) docker rm $(docker ps -aq)
 docker compose -f app.compose.yml up -d --no-deps  --force-recreate --build
-docker compose -f efk.compose.yml up -d --no-deps  --force-recreate --build
